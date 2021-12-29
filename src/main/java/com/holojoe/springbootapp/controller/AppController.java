f230f19e738ff7da3b7e42c5656f42a44e6a5cb4
Joseph Holonou <joseph.holonou@innovertec.com>
2021-12-29 18:54:00 +0100

    Add new endpoint

--- src/main/java/com/holojoe/springbootapp/controller/AppController.java
+++ src/main/java/com/holojoe/springbootapp/controller/AppController.java
@@ -1,5 +1,8 @@
 package com.holojoe.springbootapp.controller;
 
+import java.util.Date;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,11 @@ public class AppController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return "Hello World from Spring Boot";
+    }
+
+    @GetMapping("/what-is-the-time")
+    public String time() {
+        return new Date().toString();
     }
     
 }
